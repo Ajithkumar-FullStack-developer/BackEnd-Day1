ea4ad141280665b4771e5b80cec2280606a00027
Ajithkumar-FullStack-Developer <ajithjana1996@gmail.com>
2025-01-18 00:23:15 +0530

    UI instruction updated

--- index.js
+++ index.js
@@ -4,7 +4,20 @@ const { createFile, listFiles } = require('./utils/filesys');
 const app = express();
 const PORT = 3000;
 
+// Serve endpoint details at the root URL
+app.get('/', (req, res) => {
+    const html = `
+        <h1>API Endpoints</h1>
+        <ul>
+            <li><strong>POST /create-file:</strong> Creates a new text file with the current timestamp.</li>
+            <li><strong>GET /list-files:</strong> Retrieves all text files in the directory.</li>
+        </ul>
+        <p>Use tools like Postman or cURL to interact with these endpoints.</p>
+    `;
+    res.send(html);
+});
 
+// Create a text file
 app.post('/create-file', (req, res) => {
     try {
         const fileName = createFile();
@@ -15,7 +28,7 @@ app.post('/create-file', (req, res) => {
     }
 });
 
-
+// List all text files
 app.get('/list-files', (req, res) => {
     try {
         const files = listFiles();
@@ -26,7 +39,7 @@ app.get('/list-files', (req, res) => {
     }
 });
 
-
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
